258ce52d5c0e8301ed52097d04d596628c16c12a
vaishali8799 <vaishalikasondhan.8799@gmail.com>
2020-08-24 02:38:57 +0530

    Updates

---
+++ precache-manifest.add4d6389303f0c478dda018779eba6b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3f79aed8d3ae35f767fdf66ae4a394",
+    "url": "/Quizone/index.html"
+  },
+  {
+    "revision": "ace488465589075e7fcd",
+    "url": "/Quizone/static/css/2.3ec49f57.chunk.css"
+  },
+  {
+    "revision": "df52491f246fb0f0370a",
+    "url": "/Quizone/static/css/main.1e891610.chunk.css"
+  },
+  {
+    "revision": "ace488465589075e7fcd",
+    "url": "/Quizone/static/js/2.a0a41e90.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/Quizone/static/js/2.a0a41e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df52491f246fb0f0370a",
+    "url": "/Quizone/static/js/main.31e190f6.chunk.js"
+  },
+  {
+    "revision": "bbc09d1e3bc50155f3cd",
+    "url": "/Quizone/static/js/runtime-main.a5b4e701.js"
+  },
+  {
+    "revision": "385b72304aa9e0c071845f1d5b905565",
+    "url": "/Quizone/static/media/button.385b7230.mp3"
+  },
+  {
+    "revision": "b5e7acadf2f28537c01f00da3611a794",
+    "url": "/Quizone/static/media/eventually.b5e7acad.mp3"
+  },
+  {
+    "revision": "4f525aba261a73f49c1d29df0e9d908d",
+    "url": "/Quizone/static/media/jason-strull-KQ0C6WtEGlo-unsplash.4f525aba.jpg"
+  },
+  {
+    "revision": "31d47085569e772c0f57aa8ec8381a5c",
+    "url": "/Quizone/static/media/materialdesignicons-webfont.31d47085.woff"
+  },
+  {
+    "revision": "4a837d054b5f2a37170df8a275a13816",
+    "url": "/Quizone/static/media/materialdesignicons-webfont.4a837d05.eot"
+  },
+  {
+    "revision": "b0fd91bb29dcb296a9a37f8bda0a2d85",
+    "url": "/Quizone/static/media/materialdesignicons-webfont.b0fd91bb.ttf"
+  },
+  {
+    "revision": "f1997a8aba8a498fe4032e3b56e871ca",
+    "url": "/Quizone/static/media/materialdesignicons-webfont.f1997a8a.woff2"
+  },
+  {
+    "revision": "a431eaad140cb7ef5f987e6d5a22ab6f",
+    "url": "/Quizone/static/media/wrong.a431eaad.mp3"
+  }
+]);
